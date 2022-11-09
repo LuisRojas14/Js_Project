b9b107254f79ebb20812ae6e56fe8f7eb80f35e1
Luis Rojas <luisignacio1210@gmail.com>
2022-11-09 21:30:20 +0100

    adding a clock

--- java.js
+++ java.js
@@ -2,3 +2,15 @@ function thingsToDo(gym) {
     console.log('going to ' + ' gym');
 }
 thingsToDo()
+
+function newElement() {
+    var li = document.createElement ('li');
+    var inputValue = document.getElementById('myImput').ariaValueMax;
+    var t
+}
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const date = new Date();
+document.getElementById("clock").innerHTML = date.toLocaleTimeString();
+}
