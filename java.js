3e7df51e504b38a17596d44e0a458aa73a471a7a
Luis Rojas <luisignacio1210@gmail.com>
2022-11-16 20:19:06 +0100

    commit

--- java.js
+++ java.js
@@ -4,6 +4,7 @@ setInterval(myTimer, 1000);
 function myTimer() {
   const date = new Date();
 document.getElementById("clock").innerHTML = date.toLocaleTimeString();
-}
+}                                                                                                 
+    
 
-
+        
